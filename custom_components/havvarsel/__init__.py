a3242075a686c3067f0bb8109e3a94bf5fd2cd56
oyvhov <oyvhov@gmail.com>
2025-02-21 12:23:31 +0100

    Update __init__.py
    Ascync update

--- custom_components/havvarsel/__init__.py
+++ custom_components/havvarsel/__init__.py
@@ -6,9 +6,8 @@ from .const import DOMAIN
 async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
     """Set up Havvarsel from a config entry."""
     # Forward the setup to the sensor platform
-    hass.async_create_task(
-        hass.config_entries.async_forward_entry_setup(entry, "sensor")
-    )
+    await hass.config_entries.async_forward_entry_setups(entry, ["sensor"])
+
     return True
 
 async def async_unload_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
