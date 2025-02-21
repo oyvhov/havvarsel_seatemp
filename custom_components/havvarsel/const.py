6fec1f29f611222faa1c10d180f642221c853f04
oyvhov <oyvhov@gmail.com>
2025-02-21 09:38:21 +0100

    Update const.py

--- custom_components/havvarsel/const.py
+++ custom_components/havvarsel/const.py
@@ -3,5 +3,6 @@ DOMAIN = "havvarsel"
 CONF_LATITUDE = "latitude"
 CONF_LONGITUDE = "longitude"
 
-# Update every 60 minutes (3600 seconds)
-UPDATE_INTERVAL = 3600
+# Example: Update every hour
+from datetime import timedelta
+UPDATE_INTERVAL = timedelta(hours=1)
