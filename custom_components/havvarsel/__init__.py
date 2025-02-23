399435f751ac715b2ac04b414f8f62269689f8cf
oyvhov <oyvhov@gmail.com>
2025-02-23 20:47:44 +0100

    Update __init__.py

--- custom_components/havvarsel/__init__.py
+++ custom_components/havvarsel/__init__.py
@@ -7,10 +7,9 @@ async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
     """Set up Havvarsel from a config entry."""
     # Forward the setup to the sensor platform
     await hass.config_entries.async_forward_entry_setups(entry, ["sensor"])
-
     return True
 
 async def async_unload_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
     """Unload Havvarsel config entry."""
-    await hass.config_entries.async_forward_entry_unload(entry, "sensor")
-    return True
+    unload_ok = await hass.config_entries.async_forward_entry_unload(entry, "sensor")
+    return unload_ok
