0a609257576898bbc2bf596e3ae6dcda9368dfc0
oyvhov <oyvhov@gmail.com>
2025-02-23 21:07:45 +0100

    Update const.py

--- custom_components/havvarsel/const.py
+++ custom_components/havvarsel/const.py
@@ -2,7 +2,7 @@ DOMAIN = "havvarsel"
 
 CONF_LATITUDE = "Latitude"
 CONF_LONGITUDE = "Longitude"
-CONF_NAME = "Sensor Name"  
+CONF_NAME = "sensor_name"  
 
 from datetime import timedelta
 UPDATE_INTERVAL = timedelta(hours=1)
